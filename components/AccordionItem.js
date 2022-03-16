215242b3555491badc9ef5d105ff345a0f3ca646
Chris Lam <christopher.lam@grainger.com>
2022-03-16 18:54:06 -0500

    looking good

--- components/AccordionItem.js
+++ components/AccordionItem.js
@@ -27,15 +27,17 @@ const AccordionItem = ({faq, active, onToggle}) => {
                         }
                     >
 
-                        <div className="answer" alt={question}>
-                            <div id="myTable">
+                        {/*<div className="answer" alt={question}>*/}
+                        {/*<div className="answer hidden container md:flex md:flex-row md:items-center md:justify-centers w-auto" alt={question}>*/}
+                        <div className="answer hidden md:flex" alt={question}>
+                            <div id="myTable" className="flex sm:hidden">
                                 <div className="text-xs font-thin text-left">Overall Score</div>
                                 <div className="text-xs font-thin text-center">FMEA</div>
                                 <div className="text-xs font-thin text-center">Gremlin</div>
                                 <div className="text-xs font-thin text-center">Updated</div>
                                 <div className="text-xs font-thin text-center">Incidents</div>
                             </div>
-                            <div id="myTable">
+                            <div id="myTable" className="flex hidden">
                                 <div className="block">
                                     <span className="mr-1.5">{question}</span>
                                     <span className="passfail" alt={answer}>{answer}</span>
@@ -44,6 +46,26 @@ const AccordionItem = ({faq, active, onToggle}) => {
                                 <div className="text-center">{gremlin}</div>
                                 <div className="text-center">{updated}</div>
                                 <div className="text-center">{incidents}</div>
+                            </div>
+                        </div>
+
+                        <div className="md:hidden lg:hidden sm:flex md:flex flex justify-between">
+                            <div>
+                                <div className="text-md font-thin text-left">Overall Score</div>
+                                <div className="text-md font-thin text-center">FMEA</div>
+                                <div className="text-md font-thin text-center">Gremlin</div>
+                                <div className="text-md font-thin text-center">Updated</div>
+                                <div className="text-md font-thin text-center">Incidents</div>
+                            </div>
+                            <div id="">
+                                <div className="block text-right">
+                                    <span className="mr-1.5">{question}</span>
+                                    <span className="passfail" alt={answer}>{answer}</span>
+                                </div>
+                                <div className="text-right">{fmea}</div>
+                                <div className="text-right">{gremlin}</div>
+                                <div className="text-right">{updated}</div>
+                                <div className="text-right">{incidents}</div>
                             </div>
                         </div>
                     </div>
