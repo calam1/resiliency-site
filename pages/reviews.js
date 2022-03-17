1784075b76317fb52badf37763d65b59d4ea7231
Chris Lam <christopher.lam@grainger.com>
2022-03-17 10:43:49 -0500

    add some verbiage etc to reviews page

--- pages/reviews.js
+++ pages/reviews.js
@@ -14,6 +14,9 @@ function HomePage() {
 
     return (
         <div className="container">
+            <div className="text-4xl">Resiliency Reviews</div>
+            <p className="font-thin text-left pt-10 pb-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis aspernatur voluptas inventore ab voluptates nostrum minus illo laborum harum laudantium earum ut, temporibus fugiat sequi explicabo facilis unde quos corporis!</p>
+
             <Accordion />
         </div>
     );
